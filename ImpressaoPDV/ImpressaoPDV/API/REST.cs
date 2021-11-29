870911317e15b174b3d3bbd9350043c70eee6934
Guilherme Alves <guilhermealves.tecware@gmail.com>
2021-11-29 11:19:03 -0300

    first commit

---
+++ ImpressaoPDV/ImpressaoPDV/API/REST.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tecware.Printer.Enums;
+
+namespace ImpressaoPDV.API
+{
+    public class REST
+    {
+        public REST(int idOrder, AmbAPIEnum amb_API, EndPointEnum endPoint, string token)
+        {
+            IdOrder = idOrder;
+            Amb_API = amb_API;
+            Token = token;
+            End_Point = endPoint;
+        }
+
+        public int IdOrder { get; set; }
+        public string Token { get; set; }
+        public AmbAPIEnum Amb_API { get; set; }
+        public EndPointEnum End_Point { get; set; }
+
+        public enum AmbAPIEnum
+        {
+            PROD = 1,
+            DEV
+        }
+
+        public enum EndPointEnum
+        {
+            COMANDA = 1,
+            SETOR,
+            NOTA
+        }
+
+        public static Dictionary<string, string> AmbAPI = new Dictionary<string, string>
+        {
+            { "PROD", "http://api.tecware.com.br/api/"},
+            { "DEV", "https://teste-api.tecware.com.br/api/"}
+        };
+
+
+        public static Dictionary<string, string> EndPoint = new Dictionary<string, string>
+        {
+            { "COMANDA", "PDV/GerarImpressaoComanda/{_key}"},
+            { "SETOR", "Lojas/GerarImpressaoSetor/{_key}"},
+            { "NOTA", "NotasFiscais/GerarImpressaoNFe/{_key}"},
+            { "LOJA", "Lojas/LojaLogged"}
+        };
+
+        public Response Get()
+        {
+            var client = new RestClient(
+                AmbAPI.FirstOrDefault(c => c.Key == Amb_API.ToString())
+                .Value);
+
+            var request = new RestRequest(EndPoint.
+                FirstOrDefault(c => c.Key == End_Point.ToString())
+                .Value.Replace("{_key}", IdOrder.ToString()), Method.GET);
+
+            client.Authenticator = new JwtAuthenticator(Token);
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.BadRequest || response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                throw new Exception(response.Content);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                throw new Exception("Token expired!");
+
+            var layout = response.RawBytes;
+
+            request = new RestRequest("Lojas/LojaLogged", Method.GET);
+
+            response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.BadRequest || response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                throw new Exception(response.Content);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                throw new Exception("Token expired!");
+
+            var responseAPI = JsonConvert.DeserializeObject<Response>(response.Content);
+
+            if(responseAPI == null)
+                throw new Exception("Ambiente informado instável!");
+
+            responseAPI.Layout = layout;
+
+            return responseAPI;
+        }
+
+
+        public class Response
+        {
+            public bool Sucesso { get; set; }
+            public Loja Data { get; set; }
+            public byte[] Layout { get; set; }
+        }
+
+        public class Loja
+        {
+            public int Codigo { get; set; }
+            public int ExCode { get; set; }
+            public string Nome { get; set; }
+            public int Empresa { get; set; }
+            public int ListaProduto { get; set; }
+            public int ListaAtributo { get; set; }
+            public string NomeEmpresa { get; set; }
+            public int UltimaNF { get; set; }
+            public string UsuarioGO2GO { get; set; }
+            public string SenhaGO2GO { get; set; }
+            public string Logo { get; set; }
+            public string ImpressoraComanda { get; set; }
+            public PrinterType TipoImpressoraComanda { get; set; }
+            public string ImpressoraFiscal { get; set; }
+            public PrinterType TipoImpressoraFiscal { get; set; }
+        }
+    }
+}
---
+++ ImpressaoPDV/ImpressaoPDV/Program.cs
@@ -0,0 +1,125 @@
+﻿using ImpressaoPDV.API;
+using System;
+using System.Runtime.InteropServices;
+using Tecware.Printer;
+using Tecware.Printer.Enums;
+
+namespace ImpressaoPDV
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_OPEN = 5;
+
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+            // Hide window
+            ShowWindow(handle, SW_HIDE);
+
+            // Open window
+            // ShowWindow(handle, SW_OPEN);
+
+            string printerName = string.Empty;
+            string codeOrder = string.Empty;
+            string typePrinter = string.Empty;
+            string typeAction = string.Empty;
+            string token = string.Empty;
+            string amb = string.Empty;
+
+            //Example Values
+            /*args = new string[1];
+            args.SetValue("impressoestitaniumpdv:codeOrder=284401,typeAction=COMANDA,amb=PROD,token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMSIsImp0aSI6ImZiMWZmMTE5LTNiNWQtNDFjYy1iNTZiLTZmYjMxM2E2NzQzMyIsImlhdCI6IjE2MzYzODA1MTgiLCJzdWIiOiJURVNURV9PTEdBX1JJIiwidXNlciI6InRlY3dhcmUuYWRtaW4iLCJwYXNzd29yZCI6ImJvc3MiLCJjb21wYW55IjoiVEVTVEVfT0xHQV9SSSIsImxvamFJZCI6IjEwIiwidHlwZSI6IlVTVUFSSU9QQURSQU8iLCJleHAiOjE2MzY0NjY5MTgsImlzcyI6Imh0dHA6Ly90ZWN3YXJlLmNvbS5ici8ifQ.a5Ku6nZsnpOiyE0mt4Q7JGmi4lWM3i4UpwIAixCqj4I", 0);
+            */
+
+            if (args.Length == 0 || args[0] == string.Empty)
+                throw new Exception("values empty!");
+
+            foreach (var item in args[0].Replace("impressoestitaniumpdv:", string.Empty)?.Split(","))
+            {
+                var itemAndValue = item.Split("=");
+
+                switch (itemAndValue[0])
+                {
+                    case "printerName":
+                        printerName = itemAndValue[1];
+                        break;
+                    case "codeOrder":
+                        codeOrder = itemAndValue[1];
+                        break;
+                    case "token":
+                        token = itemAndValue[1];
+                        break;
+                    case "typeAction":
+                        typeAction = itemAndValue[1];
+                        break;
+                    case "typePrinter":
+                        typePrinter = itemAndValue[1];
+                        break;
+                    case "amb":
+                        amb = itemAndValue[1];
+                        break;
+                }
+            }
+
+            if (codeOrder == string.Empty ||
+                token == string.Empty ||
+                typeAction == string.Empty
+            )
+                throw new Exception($@"
+                        Values: printerName = {printerName}; 
+                        codeOrder = {codeOrder}; 
+                        typePrinter = {typePrinter}; 
+                        token = {token}
+                        typeAction = {typeAction}
+
+                  some value is empty!");
+
+            var endpoint = ((REST.EndPointEnum)Enum.Parse(typeof(REST.EndPointEnum), typeAction));
+
+            REST api = new REST(int.Parse(codeOrder), ((REST.AmbAPIEnum)Enum.Parse(typeof(REST.AmbAPIEnum), amb)), endpoint, token);
+
+            var response = api.Get();
+
+            PrinterType type;
+            if (typePrinter != null && typePrinter != string.Empty)
+                type = ((PrinterType)Enum.Parse(typeof(PrinterType), typePrinter));
+
+            if (response.Data == null)
+                throw new Exception("Loja não encontrada");
+
+            switch (endpoint)
+            {
+                case REST.EndPointEnum.COMANDA:
+                    if (response.Data.ImpressoraComanda == null || response.Data.ImpressoraComanda == string.Empty)
+                        throw new Exception("Nome da impressora comanda vazio ou nulo!");
+
+                    printerName = response.Data.ImpressoraComanda;
+                    type = response.Data.TipoImpressoraComanda;
+                    break;
+                case REST.EndPointEnum.NOTA:
+                    if (response.Data.ImpressoraFiscal == null || response.Data.ImpressoraFiscal == string.Empty)
+                        throw new Exception("Nome da impressora fiscal vazio ou nulo!");
+
+                    printerName = response.Data.ImpressoraFiscal;
+                    type = response.Data.TipoImpressoraFiscal;
+                    break;
+                default:
+                    type = PrinterType.Epson;
+                    break;
+            }
+
+            Printer printer = new Printer(printerName, type);
+
+            printer.Set(response.Layout);
+            printer.PartialPaperCut();
+            printer.PrintDocument();
+        }
+    }
+}
